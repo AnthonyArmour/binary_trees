f4bb777ffab9dade942c60f3d79913aafeca583b
AnthonyArmour <anthonydarmour@gmail.com>
2021-10-18 19:30:41 +0000

    clean up task 10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -10,7 +10,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
     int cnt = 0;
 
-	if ((!tree) || (binary_tree_is_leaf(tree)))
+	if (!tree)
 		return (0);
 	if (tree->left)
         cnt += 1;
