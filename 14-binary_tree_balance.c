3c32d6d0465d69698f6cb9578d40802c76251c97
AnthonyArmour <anthonydarmour@gmail.com>
2021-10-18 20:29:47 +0000

    betty task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -57,21 +57,21 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    int left = 0, right = 0;
+	int left = 0, right = 0;
 
-    if (tree)
-    {
-        if (tree->left)
-        {
-            left += 1;
-            left += binary_tree_height(tree->left);
-        }
-        if (tree->right)
-        {
-            right += 1;
-            right += binary_tree_height(tree->right);
-        }
-        return (left-right);
-    }
-    return (0);
+	if (tree)
+	{
+		if (tree->left)
+		{
+			left += 1;
+			left += binary_tree_height(tree->left);
+		}
+		if (tree->right)
+		{
+			right += 1;
+			right += binary_tree_height(tree->right);
+		}
+		return (left-right);
+	}
+	return (0);
 }
