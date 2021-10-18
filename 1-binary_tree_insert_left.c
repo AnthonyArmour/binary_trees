be9b88418bed37759d8b31ea26d43fc23e979b2e
AnthonyArmour <anthonydarmour@gmail.com>
2021-10-18 17:18:40 +0000

    pushing task 1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - inserts node as left child
+ * @parent: parent node
+ * @value: value to put in new node
+ * Return: pointer to new node, else NULL
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node = NULL;
+
+	if (!parent)
+		return (NULL);
+
+	node = binary_tree_node(parent, value);
+
+	if (node)
+	{
+		node->left = parent->left;
+		if (node->left)
+			node->left->parent = node;
+		parent->left = node;
+	}
+	else
+		return (NULL);
+
+	return (node);
+}
