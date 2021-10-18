699437282d16bfa42d912e2dc4968c7e012dd0e1
AnthonyArmour <anthonydarmour@gmail.com>
2021-10-18 17:35:18 +0000

    pushing task 3

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - inserts node as right child
+ * @tree: pointer to root node of tree to delete
+ * Return: void
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree->left)
+		binary_tree_delete(tree->left);
+	if (tree->right)
+		binary_tree_delete(tree->right);
+	free(tree);
+}
