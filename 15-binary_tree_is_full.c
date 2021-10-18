cffca80d0120369c211ade2c972b1fd551e75b01
AnthonyArmour <anthonydarmour@gmail.com>
2021-10-18 21:51:04 +0000

    null check task 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -10,6 +10,9 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
     int full = 1;
 
+    if (!tree)
+        return (0);
+
     if ((tree->left) && (!tree->right))
         return (0);
     else if ((!tree->left) && (tree->right))
